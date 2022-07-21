e5d30191a9b571084f02b9e7512c1aa0579136c7
Stmrlv <stomorolov.anyan@gmail.com>
2022-07-21 15:22:25 +0900

    Добавьте файлы проекта.

---
+++ UETest/UETest.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string a;
+    cin >> a;
+
+    for (int i = 0; i < a.length(); i++)
+    {
+        if (isupper(a[i]))
+            a[i] = a[i] + 32;
+    }
+
+    for (int i = 0; i <= a.length(); i++)
+    {
+        char b = a[i];
+
+        for (int j = i+1; j <= a.length(); j++)
+        {
+            if (b == a[j])
+            {
+                a[i] = ')';
+                a[j] = ')';
+            }
+        }
+        if (a[i] != ')')
+            a[i] = '(';
+    }
+    
+    cout << a;
+
+    return 0;
+}
+
